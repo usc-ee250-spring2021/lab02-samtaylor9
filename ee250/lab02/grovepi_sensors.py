d2f094c06b8c89b319da7ea9eb3ed5095559c8fc
samtaylor9 <samantrt@usc.edu>
2021-02-04 18:25:44 -0800

    edits at Erin's house

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -24,19 +24,6 @@ sys.path.append('../../Software/Python/')
 # This append is to support importing the LCD library.
 sys.path.append('../../Software/Python/grove_rgb_lcd')
 
-import grovepi
-
-# Connect the Grove Rotary Angle Sensor to analog port A2
-# SIG,NC,VCC,GND
-potentiometer = 2
-grovepi.pinMode(potentiometer,"INPUT")
-time.sleep(1)
-
-# Vcc of the grove interface is normally 5v
-grove_vcc = 5
-
-# Full value of the rotary angle is 300 degrees, as per it's specs (0 to 300)
-full_angle = 300
 
 """This if-statement checks if you are running this python file directly. That
 is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will
@@ -44,14 +31,40 @@ be true"""
 
 
 if __name__ == '__main__':
-    PORT = 4    # D4
+    setText("Samantha Taylor\nEE250 Lab 2")
+
+    # Connect the Grove Rotary Angle Sensor to analog port A2
+    # SIG,NC,VCC,GND
+    potentiometer = 2
+    grovepi.pinMode(potentiometer,"INPUT")
+    time.sleep(1)
+
+    # Vcc of the grove interface is normally 5v
+    grove_vcc = 5
+
+    # Full value of the rotary angle is 300 degrees, as per it's specs (0 to 300)
+    full_angle = 300
+
+    # Connect the Grove Ultrasonic Ranger to digital port D4
+    # SIG,NC,VCC,GND
+    ultrasonic_ranger = 4
 
     while True:
         #So we do not poll the sensors too quickly which may introduce noise,
         #sleep for a reasonable time of 200ms between each iteration.
         time.sleep(0.2)
+        # FIXME: setText_norefresh("")
 
-        # print(grovepi.ultrasonicRead(PORT))
-        # Read sensor value from potentiometer
-        sensor_value = grovepi.analogRead(potentiometer)
-        print(sensor_value)
+        # Read threshold value from potentiometer
+        threshold_value = grovepi.analogRead(potentiometer)
+        print(threshold_value)
+
+        # Measure distance to object from ultrasonic ranger
+        distance_to_object = (grovepi.ultrasonicRead(ultrasonic_ranger))
+        print(distance_to_object)
+
+        # Determines whether the object is within threshold distance
+        if threshold_value > distance_to_object:
+            setText_norefresh("%3dcm OBJ PRES\n%3dcm" % (threshold_value, distance_to_object))
+        else:
+            setText_norefresh("%3dcm         \n%3dcm" % (threshold_value, distance_to_object))
